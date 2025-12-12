e117ba04ea9398319d8c4c196a9686e2537e00a3
Mohammad Mohammadi <103150443+mohammadi-com@users.noreply.github.com>
2025-12-12 18:25:44 +0330

    Some string methods

--- hello_world.rb
+++ hello_world.rb
@@ -1,3 +1,16 @@
+name = "Mohammad"
+age = "35"
 print "Hello World!\n"
 puts "Hello Mohammad!"
 puts "Hello Jafar!"
+
+puts "Hi " + name
+
+puts "Hi".include? "H"
+puts "Hi"[0]
+puts "Salam"[1,4]
+puts "Salam".index("A")
+puts "Salam".index("a")
+puts "upcase".upcase()
+puts "DOWNCASE".downcase()
+
