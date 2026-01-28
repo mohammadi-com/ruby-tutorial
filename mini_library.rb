9ce5afbbaabadb80b2118d3447005bba20fe0e1c
Mohammad Mohammadi <103150443+mohammadi-com@users.noreply.github.com>
2026-01-28 16:18:56 +0000

    My code for mini library

---
+++ mini_library.rb
@@ -0,0 +1,49 @@
+class Book
+  attr_accessor :title, :author, :year
+
+  def initialize(title, author, year)
+    @title = title
+    @author = author
+    @year = year
+  end
+
+  def classic?
+    (Time.now.year - year) >= 50
+  end
+end
+
+module Catalog
+  include Enumerable
+end
+
+class Library
+  include Catalog
+
+  def initialize
+    @books = []
+    @title_map = Hash.new { |hash, key| hash[key] = [] }
+    @author_map = Hash.new { |hash, key| hash[key] = [] }
+  end
+
+  def each(&block)
+    @books.each(&block)
+  end
+
+  def add(book)
+    @title_map[book.title.downcase] << @books.length
+    @author_map[book.author.downcase] << @books.length
+    @books << book
+  end
+
+  def remove_by_title(title)
+    indexes = title_map.delete(title.downcase)
+    indexes&.each { |index| books.delete_at(index)}
+  end
+
+  def find_by_author(author)
+    indexes = author_map[author.downcase]
+    indexes.map { |index| @books[index]}
+  end
+  private
+  attr_accessor :books, :title_map, :author_map
+end
