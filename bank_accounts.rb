286c1d9605ec3370d289b3a66d7b35c4cff95bd2
Mohammad Mohammadi <103150443+mohammadi-com@users.noreply.github.com>
2026-02-06 18:11:45 +0000

    Bank accounts (inheritence, super, exceptions)

---
+++ bank_accounts.rb
@@ -0,0 +1,59 @@
+class InsufficientFundsError < StandardError; end
+
+class BankAccount
+  attr_reader :owner, :balance
+  
+  def initialize(owner, balance=0)
+    @owner = owner
+    @balance = balance
+  end
+
+  def deposit(amount)
+    amount_validation(amount)
+    @balance += amount
+  end
+
+  def withdraw(amount)
+    amount_validation(amount)
+    raise InsufficientFundsError if amount > balance
+    @balance -= amount
+  end
+
+  def to_s
+    "#{owner}: £#{balance}"
+  end
+
+  private
+  def amount_validation(amount)
+    raise ArgumentError if amount <= 0
+  end
+end
+
+class SavingsAccount < BankAccount
+  attr_accessor :withdrawal_fee
+
+  def initialize(owner, balance, withdrawal_fee=1.5)
+    super(owner, balance)
+    @withdrawal_fee = withdrawal_fee
+  end
+
+  def withdraw(amount)
+    super(amount+withdrawal_fee)
+  end
+end
+
+class CheckingAccount < BankAccount
+  attr_accessor :overdraft_limit
+
+  def initialize(owner, balance, overdraft_limit=100.00)
+    super(owner, balance)
+    @overdraft_limit = overdraft_limit
+  end
+
+  def withdraw(amount)
+    amount_validation(amount)
+    available = balance + overdraft_limit
+    raise InsufficientFundsError if amount > available
+    @balance -= amount
+  end
+end
